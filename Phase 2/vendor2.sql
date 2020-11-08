8c7ff4a0b35222e0f23a4603b35067846ed45292
Brandon Mohan <mohanbp@vcu.edu>
2020-11-08 01:09:09 -0500

    Filled Item, Vendor, and Warehouse Table

---
+++ Phase 2/vendor2.sql
@@ -0,0 +1,7 @@
+insert into vendor values("amazon.com", "Amazon");
+insert into vendor values("samsung.com", "Samsung");
+insert into vendor values("apple.com", "Apple");
+insert into vendor values("bestbuy.com", "Bestbuy");
+insert into vendor values("walmart.com", "Walmart");
+insert into vendor values("newegg.com", "Newegg");
+insert into vendor values("staples.com", "Staples");
---
+++ Phase 2/warehouses.sql
@@ -0,0 +1,7 @@
+insert into warehouses values(823, "amazon.com" , 12345);
+insert into warehouses values(690, "samsung.com" , 23456);
+insert into warehouses values(111, "apple.com" , 34567);
+insert into warehouses values(333, "bestbuy.com" , 89483);
+insert into warehouses values(543, "walmart.com" , 92738);
+insert into warehouses values(209, "newegg.com" , 42231);
+insert into warehouses values(989, "staples.com" , 57284);
