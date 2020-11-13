33b538dcd41d12858793caa63e231794e42ae692
Jonathan Giraud <46968282+Jogira@users.noreply.github.com>
2020-11-13 16:55:32 -0500

    Added a view for computer peripherals only.

--- Phase 2/SQL Files/itemListView.sql
+++ Phase 2/SQL Files/itemListView.sql
@@ -1,2 +1,5 @@
 create view itemListView as select itemID, warehouseID, itemName, currentPrice, category, manufacturer, url
 from stock natural join item natural join warehouses;
+
+CREATE VIEW computerPeripheralView AS SELECT itemID, warehouseID, itemName, currentPrice, category
+FROM stock NATURAL JOIN item NATURAL JOIN warehouses WHERE category = "Monitor" OR category = "Keyboard";
