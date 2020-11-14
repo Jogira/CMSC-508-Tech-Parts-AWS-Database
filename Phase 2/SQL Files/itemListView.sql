5abb9c7cd31153b908673005a75b9019298bdcf9
Jonathan Giraud <46968282+Jogira@users.noreply.github.com>
2020-11-13 19:16:06 -0500

    Added descriptions.

--- Phase 2/SQL Files/itemListView.sql
+++ Phase 2/SQL Files/itemListView.sql
@@ -1,5 +1,7 @@
+--Make a view for grabbing all of the data you'd need for listing every item.
 create view itemListView as select itemID, warehouseID, itemName, currentPrice, category, manufacturer, url
 from stock natural join item natural join warehouses;
 
+--Make a view explicitly for viewing items that you'd connect to a computer as peripherals. 
 CREATE VIEW computerPeripheralView AS SELECT itemID, warehouseID, itemName, currentPrice, category
 FROM stock NATURAL JOIN item NATURAL JOIN warehouses WHERE category = "Monitor" OR category = "Keyboard";
