b8dde8bcc4b13168038ec07d156a854c7ac079e4
Jonathan Giraud <46968282+Jogira@users.noreply.github.com>
2020-11-17 12:19:33 -0500

    Added a simple view for all items.

--- Phase 2/SQL Files/Add Views.sql
+++ Phase 2/SQL Files/Add Views.sql
@@ -5,3 +5,7 @@ from stock natural join item natural join warehouses;
 --Make a view explicitly for viewing items that you'd connect to a computer as peripherals. 
 CREATE VIEW computerPeripheralView AS SELECT itemID, warehouseID, itemName, currentPrice, category
 FROM stock NATURAL JOIN item NATURAL JOIN warehouses WHERE category = "Monitor" OR category = "Keyboard";
+
+--Make a view for information that an average customer would use, so attributes like warehouseID are not included.
+create view simpleItemView as select itemID, itemName, currentPrice, shippingPrice, series, manufacturer, companyName
+from stock natural join item natural join warehouses natural join vendor;
