0fe90084fd5010291f0fa53b6600dc1c0154023a
Jonathan Giraud <46968282+Jogira@users.noreply.github.com>
2020-11-18 11:34:03 -0500

    Triggers for updating historical high/low.

--- Phase 2/SQL Files/Add Triggers.sql
+++ Phase 2/SQL Files/Add Triggers.sql
@@ -1,4 +1,4 @@
---Returns an error if an item being added to the storage does NOT have "HDD" or "SSD" as the storage type.
+#Returns an error if an item being added to the storage does NOT have "HDD" or "SSD" as the storage type.
 delimiter |
 CREATE TRIGGER storageTypeRestrict
 Before INSERT on storage
@@ -9,7 +9,7 @@ SET MESSAGE_TEXT = 'ERROR: StorageType value is invalid.';
 end if;
 end;
 
---Returns an error if an item being added to the storage does NOT have "SATA" or "NVME" as the storage standard.
+#Returns an error if an item being added to the storage does NOT have "SATA" or "NVME" as the storage standard.
 CREATE TRIGGER storageStandardRestrict
 Before INSERT on storage
 FOR EACH ROW Begin
@@ -22,7 +22,7 @@ end;
 
 |
 
---These triggers make sure only reasonable values are added for CPU wattage. Most modern CPUs don't use wattages over 200. 
+#These triggers make sure only reasonable values are added for CPU wattage. Most modern CPUs don't use wattages over 200. 
 delimiter ~~
 CREATE TRIGGER wattageInsert
 BEFORE INSERT on CPU
@@ -42,3 +42,26 @@ begin
     update CPU set wattage = old.wattage;
 end if;
 end 
+
+
+#These triggers update the historical lows and highs if data updated to the tables, so when a new historically high/low price is entered, 
+#it automatically updates the other attributes.
+delimiter  ~~
+CREATE TRIGGER historicalLowUpdate
+AFTER UPDATE on stock
+FOR EACH ROW
+begin
+    if new.historicalLow < historicalLow then
+    update stock set historicalLow = new.historicalLow;
+end if;
+end
+
+delimiter  ~~
+CREATE TRIGGER historicalHighUpdate
+AFTER UPDATE on stock
+FOR EACH ROW
+begin
+    if new.historicalHigh > historicalHigh then
+    update stock set historicalHigh = new.historicalHigh;
+end if;
+end
