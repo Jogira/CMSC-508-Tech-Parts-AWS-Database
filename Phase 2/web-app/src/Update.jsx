5d36b7dd5218e9ce67905cb9b3a29d62da49795e
Brandon <>
2020-11-21 00:54:56 -0500

    Working progress

--- Phase 2/web-app/src/Update.jsx
+++ Phase 2/web-app/src/Update.jsx
@@ -1,9 +1,103 @@
 import React, { Component } from "react"
 import axios from "axios";
-
+import UserStore from "./UserStore.js";
+import { observer } from 'mobx-react'
 export default class Update extends Component {
 
+async componentDidMount() {
+  try {
+    let res = await fetch ('isLoggedIn', {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type':'application/json'
+      }
+    });
+    let result = await res.json();
+    if(result && result.success) {
+      UserStore.loading = false;
+      UserStore.isLoggedIn = true;
+      UserStore.username = result.username;
+    }
+    else{
+      UserStore.loading = false;
+      UserStore.isLoggedIn = false;
+    }
+    }
+    catch (e) {
+      UserStore.loading = false;
+      UserStore.isLoggedIn = false;
+    }
+  }
+
+  async doLogout() {
+    try {
+      let res = await fetch ('/logout', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type':'application/json'
+        }
+      });
+      let result = await res.json();
+      if(result && result.success) {
+        UserStore.isLoggedIn = false;
+        UserStore.username = '';
+      }
+      }
+      catch (e) {
+        console.log(e)
+      }
+    }
+
     render() {
+      if(UserStore.loading) {
+        return (
+          <div className="app">
+          <div className="container">
+          Loading, Please Wait...
+          </div>
+          </div>
+        );
+      } else {
+        if(UserStore.isLoggedIn) {
+          return (
+            <div className="app">
+            <div className="container">
+            Welcome {UserStore.username}
+            <submitButton text={'Log out'}
+            disabled={false}
+             onClick={() => this.logout()}
+            />
+            </div>
+            </div>
+          );
+        }
+
+      if(!UserStore.isLoggedIn) {
+        return (
+          <div className="app">
+          <div className="container">
+          Welcome {UserStore.username}
+          <div className="submitButton"> </div> <submitButton text={'Log out'}
+          disabled={false}
+           onClick={() => this.logout()}
+          />
+          </div>
+          </div>
+        );
+      }
+    }
+      return(
+      <div className="app">
+        <div className='container'>
+        <div className="loginForm">
+
+        </div>
+        </div>
+        </div>
+    )
+      if(0===1) {
         return (
             <div>
                 <div>
@@ -17,6 +111,8 @@ export default class Update extends Component {
                         </div>
                     </nav>
                 </div>
+
+
                 <div class="main-container">
                     <div class="options-container">
                             <div class="form-group">
@@ -49,4 +145,5 @@ export default class Update extends Component {
             </div>
         )
     }
+}
 }
