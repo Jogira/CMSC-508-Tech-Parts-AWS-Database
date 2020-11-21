6a7f4e0ec7ea26c64da19371ee9ce98bb3612f02
Jonathan Giraud <46968282+Jogira@users.noreply.github.com>
2020-11-21 01:49:46 -0500

    Documented roles.

--- Phase 2/SQL Files/setRoles.sql
+++ Phase 2/SQL Files/setRoles.sql
@@ -7,3 +7,8 @@ GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, RELOAD, PROCESS, REFERENCES,
 GRANT 'developer' TO 'amd';
 GRANT 'developer' TO 'BrandonM';
 GRANT 'developer' TO 'Zweitonegoismus';
+
+#Amazon does not allow anyone, not even the RDS creator, to give roles. Permissions were still able to be ggranted to others, but had to be granted 
+#instead of via roles. However, only people with the admin password and username are able to access the update page on our website, so nomral
+#users of the websites will only be able to search for results while logging in gives access to an admin page that allows items to be added,
+#delete, and updated via the website. 
